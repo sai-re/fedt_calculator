94697d06f7dff7d02615cdc6df844b6134b2f822
root <root@Siavoush.localdomain>
2019-05-15 22:39:32 +0100

    basic calc draft

--- app/main.js
+++ app/main.js
@@ -1,7 +1,13 @@
 let x;
 const FedtCalculator = {
     variables: {
+        num1: document.getElementById("num1-js"),
+        num2: document.getElementById("num2-js"),
+        operand: document.getElementById("operand-js"),
         numberPad: document.getElementById("number-pad-js"),
+        operandPad: document.getElementById("operand-pad-js"),
+        answer: document.getElementById("answer-js"),
+        equals: document.getElementById("submit-js"),
     },
 
     init: function() {
@@ -10,7 +16,57 @@ const FedtCalculator = {
     },
 
     calculator: function() {
+        let isSecondOperand = false;
+        let isDisabled = false;
+        let answer = 0;
 
+        const multiply = (num1, num2) => Number(num1) * Number(num2);
+        const subtract = (num1, num2) => Number(num1) - Number(num2);
+        const divide = (num1, num2) => Number(num1) / Number(num2);
+        const add = (num1, num2) => Number(num1) + (num2);
+        
+        x.numberPad.addEventListener('click', e => {
+            if (isSecondOperand === true) {
+                x.num2.innerHTML += e.target.textContent;
+            } else {
+                x.num1.innerHTML += e.target.textContent;
+            }
+        });
+        
+        x.operandPad.addEventListener('click', function handleOperand(e) {
+            isSecondOperand = true;
+            x.operand.innerHTML += e.target.textContent;
+
+            if (isDisabled === true) {
+                x.operandPad.removeEventListener('click', handleOperand);
+            }
+        });
+
+        x.equals.addEventListener('click', e => {
+            isDisabled = false;
+            
+            if (x.operand.textContent === 'x') {
+                answer = multiply(x.num1.textContent, x.num2.textContent);         
+            }
+            
+            if (x.operand.textContent === '+') {
+                answer = add(x.num1.textContent, x.num2.textContent);         
+            }
+
+            if (x.operand.textContent === '-') {
+                answer = subtract(x.num1.textContent, x.num2.textContent);         
+            }
+
+            if (x.operand.textContent === '/') {
+                answer = divide(x.num1.textContent, x.num2.textContent);         
+            }
+
+            x.answer.innerHTML = answer;
+            x.num1.innerHTML = answer
+
+            x.num2.innerHTML = "";
+            x.operand.innerHTML = "";
+        })
     }
 }
 
