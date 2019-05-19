3f9c7b6bc4a543621fa3f8d9c3b1e77718430c34
root <root@Siavoush.localdomain>
2019-05-19 20:06:50 +0100

    test if values print to screen

--- php/Calculations.php
+++ php/Calculations.php
@@ -4,7 +4,16 @@
         $file = fopen("data.csv","r");
         //parse csv fields into array
         $data = fgetcsv($file);
-
-        echo $data[1];
+        
+        //check if open is true
+        if (($file = fopen("data.csv", "r")) !== FALSE) {
+            //loop through parsed array
+            while (($data = fgetcsv($file)) !== FALSE) {
+                //loop through each value in array
+                foreach ($data as $cell) {
+                    echo $cell;
+                }
+            }
+        }
     }
 ?>
