89f9cc6d20f21280f6d284894ee61a5ca56b2bc8
root <root@Siavoush.localdomain>
2019-05-19 19:45:33 +0100

    add boiler plate for index

---
+++ php/index.php
@@ -0,0 +1,23 @@
+<?php
+    require_once 'Calculation.php'
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>CSV Calculations</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <main>
+        <section class="container">
+            <?php
+                csvToTable();
+            ?>
+        </section>
+    </main>
+</body>
+</html>
