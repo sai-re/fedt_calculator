452f92869e9168b3d12e38d5712268f1d2841689
root <root@Siavoush.localdomain>
2019-05-19 20:13:39 +0100

    add values to table cells

--- php/Calculations.php
+++ php/Calculations.php
@@ -4,16 +4,29 @@
         $file = fopen("data.csv","r");
         //parse csv fields into array
         $data = fgetcsv($file);
-        
+
         //check if open is true
         if (($file = fopen("data.csv", "r")) !== FALSE) {
+            //create table body
+            echo '<table class="csvTable">';
+
+            
             //loop through parsed array
             while (($data = fgetcsv($file)) !== FALSE) {
+                //create row tag
+                echo '<tr>';
+
                 //loop through each value in array
                 foreach ($data as $cell) {
-                    echo $cell;
+                    echo '<td class="csvTable__cell">'.$cell.'</td>';
                 }
+
+                echo '</tr>';
             }
+
+            echo '</tbody></table>';
         }
+        //close open file
+        fclose($file);
     }
 ?>
